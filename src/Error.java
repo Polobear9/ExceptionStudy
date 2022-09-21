c8e8ecc84eeb3a98d89ebc036d9eb5605c38c4e1
Polobear9 <lieams1996@gmail.com>
2022-09-21 10:33:07 +0900

    Add NullpointerException , LogicError

--- src/Error.java
+++ src/Error.java
@@ -1,4 +1,14 @@
 public class Error {
     int a;
     //b int; -- > compile error(SOV error)
+
+    public static void main(String[] args) {
+        //String[] names = null;
+        //System.out.println(names[0]); --> java Null-pointerException;
+
+        //LogicError
+        //System.out.println("3 + 5" + 3 + 5); --> Output : 3 + 535;
+
+
+    }
 }
