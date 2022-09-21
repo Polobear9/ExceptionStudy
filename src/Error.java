ba282154d86acac5bae1c755d9d4ed0e9c9c1d57
Polobear9 <lieams1996@gmail.com>
2022-09-21 10:36:33 +0900

    Add Exception explain.

--- src/Error.java
+++ src/Error.java
@@ -5,6 +5,8 @@ public class Error {
     public static void main(String[] args) {
         //String[] names = null;
         //System.out.println(names[0]); --> java Null-pointerException;
+        //Exception is are class. java.lang.Exception; ***
+        //Exception is meaning that the program is not working and the program has a problem.
 
         //LogicError
         //System.out.println("3 + 5" + 3 + 5); --> Output : 3 + 535;
